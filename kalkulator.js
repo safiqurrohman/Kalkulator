b0720020f5eba642d7d84de4259f2bdf1ee6116c
safikur1234 <safikur828@gmail.com>
2024-05-22 09:40:28 +0700

    first commit

---
+++ kalkulator.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.querySelector(".display");
+    const buttons = document.querySelectorAll(".buttons button");
+    const specialChar = ["%", "*", "/", "-", "+", "="];
+
+    let output = "";
+
+    const calculate = (btnValue) => {
+        display.focus();
+        if(btnValue === "=" && output !== " "){
+            output=eval(output.replace("%", "/100"));
+        }else if (btnValue === "AC"){
+            output = " ";
+        }else if(btnValue === "DEL"){
+            output=output.toString().slice(0, -1);
+        }else{
+            if(output === " " && specialChar.includes(btnValue)) return;
+            output+=btnValue;
+        }
+        display.value=output;
+    };
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+    });
+});
